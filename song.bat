bf9a008df14a435b2e8a503abbb3c85aae3751e8
Piotr1103 <manjudamin@gmail.com>
2021-11-01 01:30:03 +0800

    增加排序後行號印出

--- song.bat
+++ song.bat
@@ -1,12 +1,21 @@
-chcp 65001
-@setlocal enableextensions enabledelayedexpansion
 @echo off
+@chcp 65001
+@setlocal enableextensions enabledelayedexpansion
 @set /a n=0
 for /f "tokens=3 delims=	" %%i in ('Type %1^| Find "/youtu."') do (
 	@set /a "n=n+1"
 	echo !n!:	%%i>>numberedList.txt
 	echo %%i>>rawList.txt
 )
-set n=
-endlocal
 sort /uniq rawList.txt /o rawList.sorted.txt
+del rawList.txt
+@set /a m=0
+for /f %%j in (rawList.sorted.txt) do (
+	set /a "m=m+1"
+	echo !m!:	%%j>>numberedsortedList.txt
+)
+set /a "d=n-m"
+echo 原檔多出!d!行
+set n=
+set m=
+endlocal
